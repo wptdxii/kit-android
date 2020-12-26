c0f85e57584790d06e9207bef22c64d334fa9fce
wptdxii <wptdxii@gmail.com>
2020-12-26 20:26:39 +0800

    Bump to kotlin 1.4.30-M1

--- kia/src/main/java/com/rocbillow/kia/KotlinInAction.kt
+++ kia/src/main/java/com/rocbillow/kia/KotlinInAction.kt
@@ -1,1 +1,11 @@
 package com.rocbillow.kia
+
+class Person(private val firstName: String, private val lastName: String) {
+
+  override fun equals(other: Any?): Boolean {
+    val otherPerson = other as? Person ?: return false
+    return otherPerson.firstName == firstName &&
+        otherPerson.lastName == lastName
+  }
+}
+
