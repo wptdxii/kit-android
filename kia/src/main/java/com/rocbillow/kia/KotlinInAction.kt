fcf8948d186b2f3c9090a98ac0664c4df778087b
wptdxii <wptdxii@gmail.com>
2021-05-07 10:10:23 +0800

    Update dependencies

--- kia/src/main/java/com/rocbillow/kia/KotlinInAction.kt
+++ kia/src/main/java/com/rocbillow/kia/KotlinInAction.kt
@@ -1,15 +1,5 @@
 package com.rocbillow.kia
 
-import kotlinx.coroutines.CoroutineScope
-import kotlinx.coroutines.Job
-import kotlinx.coroutines.launch
-import kotlinx.coroutines.runBlocking
+fun main() {
 
-fun main(): Unit = runBlocking {
-  val job = Job()
-  val scope = CoroutineScope(job)
-
-  val j = scope.launch {
-
-  }
 }
